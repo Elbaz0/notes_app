f8c1516e37e8e2303b92b2875190dc1c57bc70d6
ibrahimElbaz <ibrahim.elbaz.1011@gmail.com>
2024-07-11 15:33:51 +0300

    create edit veiw

---
+++ lib/views/edit_notes.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/widget/edit_notes_body.dart';
+
+class edit_notes_view extends StatelessWidget {
+  const edit_notes_view({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const edit_notes_body();
+  }
+}
---
+++ lib/widget/edit_notes_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class edit_notes_body extends StatelessWidget {
+  const edit_notes_body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}

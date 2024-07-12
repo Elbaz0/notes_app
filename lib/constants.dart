84664f102b64315ebca76eff3bea9563f87d35c8
ibrahimElbaz <ibrahim.elbaz.1011@gmail.com>
2024-07-12 17:02:40 +0300

    create type_adabter  by Hive

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const Kboxname = "Notes_box";
--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,15 @@
-class notes_model {
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class notes_model extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String sub_title;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
   notes_model(

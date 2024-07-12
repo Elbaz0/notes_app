f7c1baacc26ca35e614025211dbbc808abd2cb1a
ibrahimElbaz <ibrahim.elbaz.1011@gmail.com>
2024-07-12 15:28:04 +0300

    create note_model

---
+++ lib/models/note_mode.dart
@@ -0,0 +1,12 @@
+class notes_model {
+  final String title;
+  final String sub_title;
+  final String date;
+  final int color;
+
+  notes_model(
+      {required this.title,
+      required this.sub_title,
+      required this.date,
+      required this.color});
+}

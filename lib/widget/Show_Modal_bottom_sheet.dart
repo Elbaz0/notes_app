57d03573ade12d9d02b2cf77ea75cb39082f2e09
ibrahimElbaz <ibrahim.elbaz.1011@gmail.com>
2024-07-14 11:19:49 +0300

    add add_note_cubit in show_modal_sheet

--- lib/widget/Show_Modal_bottom_sheet.dart
+++ lib/widget/Show_Modal_bottom_sheet.dart
@@ -1,77 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:note_app/widget/custem_button.dart';
-import 'package:note_app/widget/custem_text_fild.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:note_app/widget/Validate_show_modal.dart';
 
 class Show_Modal_bottom_sheet extends StatelessWidget {
   const Show_Modal_bottom_sheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: Validate_show_modal(),
-      ),
-    );
-  }
-}
-
-class Validate_show_modal extends StatefulWidget {
-  const Validate_show_modal({
-    super.key,
-  });
-
-  @override
-  State<Validate_show_modal> createState() => _Validate_show_modalState();
-}
-
-class _Validate_show_modalState extends State<Validate_show_modal> {
-  GlobalKey<FormState> formkey = GlobalKey();
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-  String? title, subtitle;
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: formkey,
-      autovalidateMode: autovalidateMode,
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 40,
-          ),
-          custem_text_fild(
-            hinttext: 'title',
-            onsave: (value) {
-              title = value;
-            },
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          custem_text_fild(
-            hinttext: 'content',
-            maxline: 5,
-            onsave: (value) {
-              subtitle = value;
-            },
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          custem_Bottom(
-            ontap: () {
-              if (formkey.currentState!.validate()) {
-                formkey.currentState!.save();
-              } else {
-                autovalidateMode = AutovalidateMode.always;
-                setState(() {});
-              }
-            },
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-        ],
+        child: BlocConsumer<AddNoteCubit, AddNoteCubitState>(
+          listener: (context, state) {
+            if (state is AddNotesuccess) {
+              return Navigator.pop(context);
+            }
+            if (state is AddNotefaild) {
+              return print('errore ${state.erroremsg}');
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              child: Validate_show_modal(),
+              inAsyncCall: state is AddNoteloading ? true : false,
+            );
+          },
+        ),
       ),
     );
   }

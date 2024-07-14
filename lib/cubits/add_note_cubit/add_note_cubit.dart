41eb7c6254b936c0690c294f180580f285ace923
ibrahimElbaz <ibrahim.elbaz.1011@gmail.com>
2024-07-14 08:33:04 +0300

    create function method cubit and registerAdapter in main

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,9 +1,12 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
+import 'package:note_app/models/note_model.dart';
 
 part 'add_note_cubit_state.dart';
 
 class AddNoteCubitCubit extends Cubit<AddNoteCubitState> {
   AddNoteCubitCubit() : super(AddNoteCubitInitial());
+
+  add_note(notes_model note) {}
 }
